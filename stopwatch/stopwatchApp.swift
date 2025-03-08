dd30574feba8fc0be2d3dfa0ff168d12732011a1
Joshua Jesse Coronado <joshua@coronado.io>
2025-03-07 17:33:55 -0800

    Initial Commit

---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by Joshua Coronado on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stopwatchTests/stopwatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchTests.swift
+//  stopwatchTests
+//
+//  Created by Joshua Coronado on 3/7/25.
+//
+
+import Testing
+@testable import stopwatch
+
+struct stopwatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
